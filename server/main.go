ee67c3b51aea7aec2540f4c88f43d248e0270455
lebrancconvas <lebrancconvas@gmail.com>
2022-05-14 09:23:37 +0700

    🔨 Update Server.

--- server/main.go
+++ server/main.go
@@ -4,17 +4,45 @@ import(
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"log"
+	"io/ioutil"
+	"net/http"
 )
+
+type Webdata struct{
+	ID int `json:"id"`
+	Name string `json:"name"`
+	URL string `json:"url"`
+}
 
 func main() {
 	fmt.Println("Hello, World!")
 
 	app := fiber.New()
 
+	// webs := []Webdata{}
+
+	res, err := http.Get("http://localhost:9000/webpolicies")
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil{
+		log.Fatal(err)
+	}
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Welcome to Web Policy Random Server.")
 	})
+
+	app.Get("/api/v1/webpolicies", func(c *fiber.Ctx) error {
+		return c.JSON(body)
+	})
+
+	app.Post("/")
 
 	log.Fatal(app.Listen(":3002"))
 }
